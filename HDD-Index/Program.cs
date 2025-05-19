16a73e1cde1cb2d92c0450317311106ade115ffa
Maodlife <64200869+maodlife@users.noreply.github.com>
2025-05-19 22:25:05 +0800

    ReactiveUI

--- HDD-Index/Program.cs
+++ HDD-Index/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using Avalonia.ReactiveUI;
 
 namespace HDD_Index;
 
@@ -17,5 +18,6 @@ sealed class Program
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .UseReactiveUI();
 }
